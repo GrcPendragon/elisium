81b76a509bd0e43a59a1ac3f95c2c7253b6e138d
Fausto Garcia <dev.faustogarcia@gmail.com>
2022-06-13 20:36:21 -0500

    Se agregan comentarios de documentacion y los metodos para generar password alfanumercas con caracteres especiales. Falta agregar al menu la generacion de password con caracteres especiales.

--- src/main/java/com/testingdevops/elisium/Elisium.java
+++ src/main/java/com/testingdevops/elisium/Elisium.java
@@ -20,20 +20,20 @@ public class Elisium {
                           2) Code PIN with variable length.
                           3) Password alphanumeric.
                           4) Password alphanumeric with variable length.
+                          5) Password alphanumeric with special characters.
+                          6) Password alphanumeric with variable length and special characters.
                          Option -> """);
         opt = keyboard.nextInt();
         switch (opt) {
-            case 1 -> {
+            case 1 ->
                 System.out.println("Your new PIN is: " + pass.getPin());
-            }
             case 2 -> {
                 System.out.print("Insert the digits amount to PIN code -> ");
                 length = keyboard.nextInt();
                 System.out.println("Your new PIN code is: " + pass.getPin(length));
             }
-            case 3 -> {
+            case 3 ->
                 System.out.println("Your new password is: " + pass.getPassword());
-            }
             case 4 -> {
                 System.out.print("Insert the characters number to password -> ");
                 length = keyboard.nextInt();
--- src/main/java/com/testingdevops/elisium/Password.java
+++ src/main/java/com/testingdevops/elisium/Password.java
@@ -7,6 +7,7 @@ import java.util.Random;
  * @author Fausto Garcia
  */
 public class Password {
+
     private final String NUMBER = "0123456789";
     private final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
     private final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -16,66 +17,141 @@ public class Password {
     private final StringBuilder pass;
     private final Random roulette;
 
+    /**
+     * Este constructor asigna a memoria los objetos Ramdom y la cada pass
+     */
     public Password() {
         roulette = new Random();
-        this.pass = new  StringBuilder();
+        this.pass = new StringBuilder();
     }
-    
+
+    /**
+     * Este metodo genera una cadena de 4 digitos numericos.
+     *
+     * @return retorna una cadena con el codigo PIN generado
+     */
     public String getPin() {
         for (int i = 0; i < PIN_LENGHT; i++) {
             this.pass.append(getNumber());
         }
         return this.pass.toString();
     }
-    
-    public String getPin(int pin_Length){
+
+    /**
+     * Este metodo genera una cadena de digitos variables, segun los pasados por
+     * parametros.
+     *
+     * @param pin_Length
+     * @return retorna una cadena con el codigo PIN generado.
+     */
+    public String getPin(int pin_Length) {
         for (int i = 0; i < pin_Length; i++) {
             this.pass.append(getNumber());
         }
         return this.pass.toString();
     }
-    
-    public String getPassword (){
+
+    /**
+     * Este metodo genera una password con una longitud de 8 caracteres
+     * alfanumericos
+     *
+     * @return retorna una cadena con la password de 8 caracteres.
+     */
+    public String getPassword() {
         int opt;
         for (int i = 0; i < PASS_LENGHT; i++) {
-            opt = roulette.nextInt(1,3);
+            opt = roulette.nextInt(1, 3);
             switch (opt) {
-                case 1 -> this.pass.append(getNumber());
-                case 2 -> this.pass.append(getLowercase());
-                case 3 -> this.pass.append(getUppercase());
-                default -> throw new AssertionError();
+                case 1 ->
+                    this.pass.append(getNumber());
+                case 2 ->
+                    this.pass.append(getLowercase());
+                case 3 ->
+                    this.pass.append(getUppercase());
+                default ->
+                    throw new AssertionError();
             }
         }
         return this.pass.toString();
     }
-    
-    public String getPassword (int pass_Lenght){
+
+    public String getPasswordSpecial() {
+        int opt;
+        for (int i = 0; i < PASS_LENGHT; i++) {
+            opt = roulette.nextInt(1, 4);
+            switch (opt) {
+                case 1 ->
+                    this.pass.append(getNumber());
+                case 2 ->
+                    this.pass.append(getLowercase());
+                case 3 ->
+                    this.pass.append(getUppercase());
+                case 4 ->
+                    this.pass.append(getSpecial());
+                default ->
+                    throw new AssertionError();
+            }
+        }
+        return this.pass.toString();
+    }
+
+    /**
+     * Este metodo genera una password con una longitud variable, alfanumerica.
+     *
+     * @param pass_Lenght
+     * @return retorna una cadena con la password generada.
+     */
+    public String getPassword(int pass_Lenght) {
         int opt;
         for (int i = 0; i < pass_Lenght; i++) {
             opt = roulette.nextInt(1, 3);
             switch (opt) {
-                case 1 -> this.pass.append(getNumber());
-                case 2 -> this.pass.append(getLowercase());
-                case 3 -> this.pass.append(getUppercase());
-                default -> throw new AssertionError();
+                case 1 ->
+                    this.pass.append(getNumber());
+                case 2 ->
+                    this.pass.append(getLowercase());
+                case 3 ->
+                    this.pass.append(getUppercase());
+                default ->
+                    throw new AssertionError();
             }
         }
         return this.pass.toString();
     }
-    
-    public char getNumber() {
+
+    public String getPasswordSpecial(int pass_Lenght) {
+        int opt;
+        for (int i = 0; i < pass_Lenght; i++) {
+            opt = roulette.nextInt(1, 4);
+            switch (opt) {
+                case 1 ->
+                    this.pass.append(getNumber());
+                case 2 ->
+                    this.pass.append(getLowercase());
+                case 3 ->
+                    this.pass.append(getUppercase());
+                case 4 ->
+                    this.pass.append(getSpecial());
+                default ->
+                    throw new AssertionError();
+            }
+        }
+        return this.pass.toString();
+    }
+
+    private char getNumber() {
         return NUMBER.charAt(roulette.nextInt(1, 10));
     }
 
-    public char getLowercase() {
+    private char getLowercase() {
         return LOWERCASE.charAt(roulette.nextInt(1, 26));
     }
 
-    public char getUppercase() {
+    private char getUppercase() {
         return UPPERCASE.charAt(roulette.nextInt(1, 26));
     }
 
-    public char getSpecial() {
+    private char getSpecial() {
         return SPECIAL.charAt(roulette.nextInt(1, 26));
     }
 }
